2a33c9903180bbb3334ca7c227c9586b436506cc
H2 <haakon@gurusoft.no>
2018-01-17 12:19:06 +0100

    Updated, implemented init action, creating 3 Person instances.

--- grails-app/init/h2gr/grails/fields/plugin/experiment/BootStrap.groovy
+++ grails-app/init/h2gr/grails/fields/plugin/experiment/BootStrap.groovy
@@ -3,7 +3,12 @@ package h2gr.grails.fields.plugin.experiment
 class BootStrap {
 
     def init = { servletContext ->
+        new Person(name:'First Person').save(failOnError:true)
+        new Person(name:'Second Person').save(failOnError:true)
+        new Person(name:'Third Person').save(failOnError:true)
     }
+
     def destroy = {
     }
+
 }
